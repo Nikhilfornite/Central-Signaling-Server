81ae11058b9af849e88318fe79a4d158ae482ffb
arjun055 <nagarjunarjun711@gmail.com>
2024-07-03 20:04:01 +0530

    added dotenv

--- Server/server.js
+++ Server/server.js
@@ -12,11 +12,11 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const server = createServer(app);
 
-app.use(express.static(path.join(__dirname, '..', 'client')));
+// app.use(express.static(path.join(__dirname, '..', 'client')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+// });
 
 const io = new Server(server,{
     cors:{
