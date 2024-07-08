a8c9f2bd0bfc3891d43cee662f9882140898f5f4
Nikhilfornite <nikhil.learnpy@gmail.com>
2024-07-08 21:04:44 +0530

    Queries for table added

---
+++ Authentication/query.sql
@@ -0,0 +1,17 @@
+create table users(
+  id UUID default gen_random_uuid() primary key,
+  user_name varchar(50) unique,
+  user_email varchar(50) unique,
+  user_password varchar(100) unique
+);
+
+create table user_history(
+  log_id UUID references users(id),
+  call_date DATE,
+  call_time timestamptz,
+  primary key (log_id,call_date,call_time)
+);
+
+create table rooms{
+    roomid varchar(255) primary key
+};
